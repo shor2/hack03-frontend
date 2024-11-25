1ae2cdc939ed6d2c64b18930b10b6411fac9d8c3
Stephane HOR <stephane.hor@gmail.com>
2024-11-25 16:05:12 +0100

    BACKEND_SERVER variable cree

--- components/Trends.js
+++ components/Trends.js
@@ -10,7 +10,7 @@ function Trends() {
   const [trendsData, setTrendsData] = useState([]);
 
   useEffect(() => {
-    fetch(`http://localhost:3000/tweets/trends/${user.token}`)
+    fetch(`http://${BACKEND_SERVER}:3000/tweets/trends/${user.token}`)
       .then(response => response.json())
       .then(data => {
         data.result && setTrendsData(data.trends);
